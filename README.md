d276aa9b49a55ee2215bc32d02bcb6374e0cd1e5
vishal1132 <vishals@iitbhilai.ac.in>
2020-09-03 19:00:06 +0530

    slack secrets deploy testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repo
+
+This repo is solely for testing github actions and then deploying those actions in other repos.
--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("learning github actions")
 	fmt.Println("tessting gitbot")
 	fmt.Println("now testing our custom message")
+	fmt.Println("testing slack secret deployed")
 }
